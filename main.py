2a4912d63904b2e9aabde17b70de6ddeafd8ba29
Saturn-VI <112597058+Saturn-VI@users.noreply.github.com>
2025-03-31 14:53:17 -0500

    Clean up main, clean up output, fix extra arc issue

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import operator
 import functools
 from ezdxf import edgesmith, edgeminer
 
-doc = ezdxf.readfile("test3.dxf")
+print("""
+Notes:
+- needs DXF version 2000
+""")
+doc = ezdxf.readfile(input("filename of input dxf"))
+outfilename = (in := input("filename of output dxf")).endswith(".dxf") ? in : in + ".dxf"
 modelspace = doc.modelspace()
+to_delete = []
 
 # get all entities in the document
+print("*** Getting entities in document. ***")
 entities = modelspace.entity_space
 print(f"Entities found: {[str(entity) for entity in entities]}")
 
@@ -30,37 +37,41 @@ def counterclockwise_angle_degrees(v1, v2):
         angle_degrees = 360 - math.degrees(angle_radians)
     return angle_degrees
 
+
+
 # process circles/flattened into arcs
-to_delete = []
-print("processing circles")
+print("*** Processing circles. ***")
 for entity in entities:
     if "CIRCLE" in str(entity):
-        print(f"found circle {str(entity)}")
+        print(f"Found circle {str(entity)}.")
         to_delete.append(entity)
         flattened = list(entity.flattening(entity.dxf.radius / 3.0))
-        print(f"handling {len(flattened)} points")
+        # print(f"handling {len(flattened)} points")
         circlecenter = entity.dxf.center
-        for index, _ in enumerate(flattened):
+        # start and end point in flattened are equal, so we stop it early
+        # see https://ezdxf.readthedocs.io/en/stable/dxfentities/circle.html#ezdxf.entities.Circle.flattening
+        for index, _ in enumerate(flattened[:-1]):
             # points are returned CCW order
             p1 = flattened[index]
-            p2 = flattened[(index + 1) % len(flattened)]
+            p2 = flattened[index + 1]
             v1 = [circlecenter[0] - p1[0], circlecenter[1] - p1[1]]
             v2 = [circlecenter[0] - p2[0], circlecenter[1] - p2[1]]
             v_east = [1, 0]
             a1 = counterclockwise_angle_degrees(v_east, v1)
             a2 = counterclockwise_angle_degrees(v_east, v2)
             modelspace.add_arc(center=entity.dxf.center, radius=entity.dxf.radius, start_angle=a1, end_angle=a2)
-print("done processing circles")
+print("*** Done processing circles. ***")
 
+
+
+print("*** Processing loops & creating polylines. ***")
 # get all edges (i.e. non-circles (?))
 edges = list(edgesmith.edges_from_entities_2d(entities))
 print(f"Edges found: {edges}")
-
 # find all closed loops
 deposit = edgeminer.Deposit(edges)
 loops = edgeminer.find_all_loops(deposit)
 print(f"Loops found: {loops}")
-
 # so we know what to not add when recreating the dxf
 items_to_ignore = set()
 outloops = []
@@ -69,23 +80,19 @@ for l in loops:
 for edge in list(outloops):
     items_to_ignore.add(str(edge.payload))
 print(f"{len(items_to_ignore)} edges now unnecessary: {items_to_ignore}")
-
 # create the polylines
-# new_polylines = [edgesmith.polyline_path_from_chain(loop) for loop in loops]
-# new_polylines = [edgesmith.polyline2d_from_chain(loop) for loop in loops]
 new_polylines = [edgesmith.lwpolyline_from_chain(loop) for loop in loops]
 print(f"Polylines created: {[str(line) for line in new_polylines]}")
-# for polyline in new_polylines:
-#     print(polyline.get_points())
 
 
+
+print("*** Final cleanup (deleting unnecessary entities, closing polylines, etc). ***")
 # edit the dxf
 for entity in entities:
     if str(entity) in items_to_ignore:
         to_delete.append(entity)
 for item in to_delete:
     modelspace.delete_entity(item)
-
 for polyline in new_polylines:
     # force the polyline to be closed
     polyline.close()
@@ -93,7 +100,9 @@ for polyline in new_polylines:
     # add polyline to modelspace
     # modelspace.add_polyline2d(polyline.points(), close=True)
     modelspace.add_lwpolyline(polyline.get_points())
+print(f"Remaining entities in entity space: {[*modelspace.entity_space]}")
 
-print(f"Entities in entity space: {[*modelspace.entity_space]}")
 
-doc.saveas("out.dxf")
+
+print(f"*** Saving file at {outfilename}. ***")
+doc.saveas(outfilename)
